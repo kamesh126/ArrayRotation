15ebff33dc055bb58d87184c7a7e6f5dd713ceaa
kamesh <kameshkumaar12612@gmail.com>
2023-07-13 17:39:01 +0530

    ArrayRotation.java

---
+++ ArrayRotation.java
@@ -0,0 +1,35 @@
+public class ArrayRotation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int array[] = new int[size];
+        for(int i=0;i<size;i++)
+        {
+            array[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        rotateArray(array,0, array.length-1);
+        rotateArray(array,0,k-1);
+        rotateArray(array,k, array.length-1);
+
+        for(int result : array)
+        {
+            System.out.print(result+" ");
+        }
+
+    }
+    public static void rotateArray(int[] array,int start,int end)
+    {
+        int i = start;
+        int j = end;
+        while(i<j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+}
